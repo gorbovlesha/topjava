b140cc0d96f291e2f70b012acf0b3996c8a86941
gorbovlesha <gorbovalexey7@gmail.com>
2021-06-08 16:27:08 +0300

    created a table of meals & added jstl

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -6,8 +6,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.MealTo;
+import ru.javawebinar.topjava.util.MealsUtil;
 
 import java.io.IOException;
+import java.util.List;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
@@ -19,6 +23,8 @@ public class MealServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         log.debug("redirect to meals");
 
-        resp.sendRedirect("meals.jsp");
+        List<MealTo> mealsList = MealsUtil.getMealsDTO();
+        req.setAttribute("mealsList", mealsList);
+        req.getRequestDispatcher("/meals.jsp").forward(req, resp);
     }
 }
--- src/main/webapp/meals.jsp
+++ src/main/webapp/meals.jsp
@@ -1,4 +1,5 @@
-<%@ page contentType="text/html;charset=UTF-8" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <html lang="ru">
 <head>
     <title>Meals</title>
@@ -7,5 +8,21 @@
 <h3><a href="index.html">Home</a></h3>
 <hr>
 <h2>Meals</h2>
+
+<table>
+<tr>
+    <th>Date</th>
+    <th>Description</th>
+    <th>Calories</th>
+</tr>
+    <c:forEach var="meals" items="${requestScope.mealsList}">
+        <tr>
+<td>${meals.dateTime}</td>
+<td>${meals.description}</td>
+<td>${meals.calories}</td>
+        </tr>
+    </c:forEach>
+
+</table>
 </body>
 </html>
